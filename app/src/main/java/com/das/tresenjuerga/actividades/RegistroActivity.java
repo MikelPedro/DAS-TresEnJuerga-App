a785e9ae2a5799320449ce25cdc4e81fd9249315
Ivan <ivanmatafernandez@gmail.com>
2024-04-25 21:42:10 +0200

    Crear actividad padre y automatizar carga de layouts

--- app/src/main/java/com/das/tresenjuerga/actividades/RegistroActivity.java
+++ app/src/main/java/com/das/tresenjuerga/actividades/RegistroActivity.java
@@ -2,17 +2,15 @@ package com.das.tresenjuerga.actividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.das.tresenjuerga.R;
 
-public class MainActivity extends ActividadPadre {
+public class RegistroActivity extends ActividadPadre {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.setContentView(R.layout.activity_main);
-
+        super.setContentView(R.layout.activity_registro);
     }
 }
